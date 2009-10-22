edf67bbf5153a19eefa81f40ed2a8b56b7dc2f97
Brian Cassidy <bricas@cpan.org>
2009-10-21 22:23:12 -0300

    add HTML (TT) view, add deps to Makefile.PL

---
+++ lib/SixteenColors/View/HTML.pm
@@ -0,0 +1,32 @@
+package SixteenColors::View::HTML;
+
+use strict;
+use warnings;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+SixteenColors::View::HTML - TT View for SixteenColors
+
+=head1 DESCRIPTION
+
+TT View for SixteenColors. 
+
+=head1 SEE ALSO
+
+L<SixteenColors>
+
+=head1 AUTHOR
+
+Brian Cassidy,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
