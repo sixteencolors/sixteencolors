147128576af20ca4b352ebd3e56856e8f333bda2
Brian Cassidy <bricas@cpan.org>
2010-02-04 16:51:28 -0400

    update scripts

--- script/sixteencolors_test.pl
+++ script/sixteencolors_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'SixteenColors';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('SixteenColors', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ sixteencolors_test.pl - Catalyst Test
 sixteencolors_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    sixteencolors_test.pl http://localhost/some_action
