3f9d797561f44085cfbd0fed3ab8f1cb917060bb
Brian Cassidy <bricas@cpan.org>
2010-12-17 11:37:24 -0400

    scripts are more permanent than "tmp" suggests. modify indexing script to warn on duplicates.

---
+++ etc/index_packs.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib';
+
+use File::Basename ();
+use SixteenColors;
+
+my $c     = 'SixteenColors';
+my @files = @ARGV;
+my $rs    = $c->model( 'DB::Pack' );
+
+for my $file ( @files ) {
+    printf "Indexing %s\n", $file;
+
+    my $basename = File::Basename::basename( $file );
+    if ( $rs->search( { filename => $basename } )->count ) {
+        printf
+            "[ERROR] A file of the same name (%s) has already been indexed\n",
+            $basename;
+        next;
+    }
+
+    $rs->new_from_file( $file, $c );
+}
