ae3460bae08d7350aa29cd8a59f62ab3a6990ab1
Brian Cassidy <bricas@cpan.org>
2011-01-18 20:18:36 -0400

    reverse scroll on click

--- root/static/js/pack.js
+++ root/static/js/pack.js
@@ -1,5 +1,6 @@
 var scroll_e;
 var scroll_t;
+var scroll_d;
 
 $( document ).ready( function() {
     $('#pack').masonry( { columnWidth: 8, itemSelector: 'h2,li,div' } );
@@ -7,11 +8,15 @@ $( document ).ready( function() {
     $('#pack li,#prev,#next').filter('[class!=noscroll]').mouseenter(
         function() {
             scroll_e = this;
+            scroll_d = -1;
             scroll_t = setTimeout( 'scrollBg()', 10 );
         }    
     )
     .mouseleave(
         function() { clearTimeout( scroll_t ); }
+    )
+    .click(
+        function() { scroll_d *= -1; }
     );
 } );
 
@@ -22,7 +27,7 @@ function scrollBg() {
         val = '0 0px';
     }
 
-    val = val.replace( /(-?\d+)px$/, function( str, p1, offet, s ){ return ( parseInt( p1 ) - 1 ) + "px"; } );
+    val = val.replace( /(-?\d+)px$/, function( str, p1, offet, s ){ return ( parseInt( p1 ) + scroll_d ) + "px"; } );
     $(scroll_e).css( "background-position", val ); 
     scroll_t = setTimeout( 'scrollBg()', 10 );
 }
