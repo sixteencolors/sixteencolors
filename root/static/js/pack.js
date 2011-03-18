bc2f9c0b88d0c2552bf24feebb6baf28421f55dc
Doug Moore <lordscarlet@idledreams.net>
2011-03-18 16:49:42 -0400

    Scrolling now stops at the extremes

--- root/static/js/pack.js
+++ root/static/js/pack.js
@@ -1,6 +1,7 @@
 var scroll_e;
 var scroll_t;
 var scroll_d;
+var scroll_h;
 
 $( document ).ready( function() {
     $('#pack').masonry( { columnWidth: 8, itemSelector: 'h2,li,div' } );
@@ -20,14 +21,52 @@ $( document ).ready( function() {
     );
 } );
 
-function scrollBg() {
-    var val = $(scroll_e).css( "background-position" );
+function getImageHeight() {
+	var pattern=/\"|\'|\)|\(|url/g;
+	var url = $(scroll_e).css('background-image').replace(pattern,'');
+	var img = $('<img />');
+	img.attr("src", url);
+	img.hide();
+	img.load(function() {
+		scroll_h = $(this).height() - $(scroll_e).height();
+		//console.log(height);
+	});
+	$("#pack").append(img);
+}
 
+function scrollBg() {
+	var match = /(-?\d+)px$/;
+    var val = $(scroll_e).css( "background-position" );
+	var top = 0;
+	if (match.exec(val))
+		top = match.exec(val)[1];
+	
+	getImageHeight();
+	console.log(scroll_h + ": " + top);
     if( /%/.test( val ) ) {
         val = '0 0px';
-    }
-
-    val = val.replace( /(-?\d+)px$/, function( str, p1, offet, s ){ return ( parseInt( p1 ) + scroll_d ) + "px"; } );
+    } 
+	if ((top * -1 >= scroll_h && scroll_d == -1) || top == 0 && scroll_d == 1) {
+		val = val;
+	} else {
+    	val = val.replace( match, function( str, p1, offet, s ){ return ( parseInt( p1 ) + scroll_d ) + "px"; } );
+	}
     $(scroll_e).css( "background-position", val ); 
     scroll_t = setTimeout( 'scrollBg()', 10 );
+	
+//	console.log(match.exec(val)[1]);
+	
+	/*
+	console.log("Background Image:" + $("<img src=\"images/menu-"+name+"-h.png\" />").attr('src',function(){
+	    return $(scroll_e).css('background-image').replace(pattern,'');}).attr("src"));
+	
+
+	$("<img src=\"images/menu-"+name+"-h.png\" />").attr('src',function(){
+	    return $(scroll_e).css('background-image').replace(pattern,'');
+	}).load(function(){
+	    w = $(this).width();
+	    h = $(this).height();
+		console.log($(this).attr("src") + ": " + h + "/" + w);
+	});
+	*/
 }
