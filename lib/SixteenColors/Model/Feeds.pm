08957cd8985c1d58b402ca3398eaee11fbfde126
Doug Moore <lordscarlet@idledreams.net>
2011-03-22 00:16:40 -0400

    Added news feed to home page. Catalyst::Model::XML::Feed is required. I need to figure out how to format the date and how to limit the number of feeds that are displayed. There is also some visual modifications that should be made, particularly for long entries.

---
+++ lib/SixteenColors/Model/Feeds.pm
@@ -0,0 +1,23 @@
+package SixteenColors::Model::Feeds;
+use base qw(Catalyst::Model::XML::Feed);
+
+=head1 NAME
+
+SixteenColors::Model::Feeds - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Doug Moore,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+1;
