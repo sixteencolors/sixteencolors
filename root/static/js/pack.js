cd997a54d7ecb730a470fa4e42f1a77da73cddb9
Doug Moore <lordscarlet@idledreams.net>
2011-03-21 21:01:26 -0400

    Cleaned up unused code

--- root/static/js/pack.js
+++ root/static/js/pack.js
@@ -1,8 +1,3 @@
-var scroll_e;
-var scroll_t;
-var scroll_d;
-var scroll_h;
-var scroll_i;
 var items = new Array();
 
 $( document ).ready( function() {
@@ -11,7 +6,6 @@ $( document ).ready( function() {
     $('#pack li').each(function() {
         items.push(new PackItem(items.length));
         items[items.length-1].getHeightFromElement(this);
-//          $(this).css("height", items[items.length-1].height + "px");
     });
      
     $('#pack li,#prev,#next').filter('[class!=noscroll]').mouseover(function(){
@@ -56,34 +50,9 @@ PackItem.prototype.getHeightFromElement = function(ele) {
 
     img.load({index:this.index, boxHeight:$(ele).height()},function(event) {
         items[event.data.index].height = $(this).height();
-        // $(ele).children("a").css("height", $(this).height() + "px");
     });
     $("#pack").append(img);
 
-    //this.height = height;
     this.src = url;
-}
-
-function scrollBg() {
-    var match = /(-?\d+)px$/;
-    var val = $(scroll_e).css( "background-position" );
-    if( /%/.test( val ) ) {
-        val = '0 0px';
-    } 
-    var max = 4;
-    var item = items[scroll_i];
-    if (match.exec(val))
-        item["top"] = match.exec(val)[1];
-
-    var start = item["top"] * -1;
-    var stop = scroll_d < 0 ? item["height"] : 0;
-    distance = stop > start ? stop - start : start - stop;
-    var step = Math.round((distance < item["height"] ? distance : item["height"] - distance + 20) / 20);
-    if (step >= max) step = max;
-    val = val.replace( match, function( str, p1, offet, s ){ return ( parseInt( p1 ) + scroll_d * step ) + "px"; } );
-    if (match.exec(val))
-        items[scroll_i]["top"] = match.exec(val)[1];
-    $(scroll_e).css( "background-position", val ); 
-    scroll_t = setTimeout( 'scrollBg()', 10 );
 }
 
