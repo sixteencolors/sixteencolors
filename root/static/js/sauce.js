6f926e94df0257c4695b364e701e8c0381d3a1ff
Doug Moore <lordscarlet@idledreams.net>
2011-03-29 21:58:12 -0400

    Disqus comments closes #2

--- root/static/js/sauce.js
+++ root/static/js/sauce.js
@@ -15,7 +15,7 @@ $( document ).ready( function() {
                 $('#disqus_container>h3').html( 'Hide discussion <span>&uarr;</span>' );
             }
             else {
-                $('#disqus_container>h3').html( 'Discuss this image <span>&darr;</span>' );
+                $('#disqus_container>h3').html( 'View discussion<span>&darr;</span>' );
             }
         } );
     } );
