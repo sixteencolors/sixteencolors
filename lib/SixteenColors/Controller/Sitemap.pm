7f0524bf87cc517e0387753cb2c0b5d4545fcd93
Doug Moore <lordscarlet@idledreams.net>
2011-04-01 19:49:21 -0400

    Added caching

--- lib/SixteenColors/Controller/Sitemap.pm
+++ lib/SixteenColors/Controller/Sitemap.pm
@@ -25,18 +25,21 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 	my $pages = ceil($c->model('DB::File')->count / 50000) ;
+	$c->cache_page();
 
 	$c->stash(no_wrapper=>1, pages => $pages);
 }
 
 sub pack :Path('pack') {
     my ( $self, $c ) = @_;
+	$c->cache_page();
     my $packs = $c->model('DB::Pack')->search( {}, { order_by => 'year DESC, month DESC, canonical_name' } );
 	$c->stash(no_wrapper=>1, packs => $packs);
 }
 
 sub file :Path('file'):Args(0) {
     my ( $self, $c ) = @_;
+	$c->cache_page();
 	my $files = $c->model( 'DB::File' )->search(
         {}, {page => $c->req->params->{ page } || 1, rows => 50000 }
     );
