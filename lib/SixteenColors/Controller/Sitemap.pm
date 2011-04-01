8b4a3a715507f71491f5df349988abb809737e29
Doug Moore <lordscarlet@idledreams.net>
2011-04-01 19:35:11 -0400

    Sitemaps added closes #26

--- lib/SixteenColors/Controller/Sitemap.pm
+++ lib/SixteenColors/Controller/Sitemap.pm
@@ -1,6 +1,7 @@
 package SixteenColors::Controller::Sitemap;
 use Moose;
 use namespace::autoclean;
+use POSIX;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
@@ -23,10 +24,28 @@ Catalyst Controller.
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-$c->stash(no_wrapper=>1);
-	
+	my $pages = ceil($c->model('DB::File')->count / 50000) ;
+
+	$c->stash(no_wrapper=>1, pages => $pages);
 }
 
+sub pack :Path('pack') {
+    my ( $self, $c ) = @_;
+    my $packs = $c->model('DB::Pack')->search( {}, { order_by => 'year DESC, month DESC, canonical_name' } );
+	$c->stash(no_wrapper=>1, packs => $packs);
+}
+
+sub file :Path('file'):Args(0) {
+    my ( $self, $c ) = @_;
+	my $files = $c->model( 'DB::File' )->search(
+        {}, {page => $c->req->params->{ page } || 1, rows => 50000 }
+    );
+
+	
+
+	$c->stash(no_wrapper=>1, files => $files);
+
+}
 
 =head1 AUTHOR
 
