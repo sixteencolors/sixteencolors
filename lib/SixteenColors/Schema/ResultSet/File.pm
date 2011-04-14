069be078dcfad5f11f4ec7ca8a95436dd65eeb8d
Doug Moore <lordscarlet@idledreams.net>
2011-04-14 16:03:26 -0400

    Added some more data

--- lib/SixteenColors/Schema/ResultSet/File.pm
+++ lib/SixteenColors/Schema/ResultSet/File.pm
@@ -14,9 +14,22 @@ sub random {
     $self->search( {}, { rows => 5, order_by => 'RANDOM()' } );
 }
 
-sub TO_JSON {
-	my $self = shift;
-	return {return [  map { { filename => $_->filename } } $self->all ] };
-}
+sub TO_JSON { 
+	my $self = shift; 
+	return [
+	   map {
+	       my $p = $_->pack;
+	       my $uri = join( '/', '/pack', $p->canonical_name, $_->filename );
+	       {
+	           filename  => $_->filename,
+	           thumbnail => "$uri/preview",
+	           fullsize  => "$uri/fullscale",
+                  uri => $uri,				
+                  pack => {filename => $p->filename, name=>$p->canonical_name, uri => join( '/', '/pack', $p->canonical_name)}
+	       }
+	   } $self->all
+	];
+} 
+
 
 1;
