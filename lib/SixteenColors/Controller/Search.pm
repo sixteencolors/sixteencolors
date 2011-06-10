348c818ec5d013d762d0c5bbec9660932a4bba8c
Brian Cassidy <bricas@cpan.org>
2011-06-10 11:33:46 -0300

    fix search form action, add blog css, move pod in search controller.

--- lib/SixteenColors/Controller/Search.pm
+++ lib/SixteenColors/Controller/Search.pm
@@ -1,24 +1,11 @@
 package SixteenColors::Controller::Search;
+
 use Moose;
 use namespace::autoclean;
 
-BEGIN { extends 'Catalyst::Controller'; }
-
-=head1 NAME
-
-SixteenColors::Controller::Search - Catalyst Controller
-
-=head1 DESCRIPTION
-
-Catalyst Controller.
-
-=head1 METHODS
-
-=cut
-
-=head2 index
-
-=cut
+BEGIN {
+    extends 'Catalyst::Controller';
+}
 
 sub index : Path : Args(0) {
     my ( $self, $c ) = @_;
@@ -72,6 +59,24 @@ sub index : Path : Args(0) {
     );
 }
 
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 NAME
+
+SixteenColors::Controller::Search - Catalyst Controller
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 index
+
 =head1 AUTHOR
 
 Sixteen Colors <contact@sixteencolors.net>
@@ -82,7 +87,3 @@ This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
-__PACKAGE__->meta->make_immutable;
-
-1;
