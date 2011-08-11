e13d17c47ab761437d4ef6f65b2099400f81e57a
Brian Cassidy <bricas@cpan.org>
2011-08-11 16:04:07 -0300

    enhance the indexing script to allow for an archive-by-year dir or a year+dir combo to be specified.

--- etc/index_packs.pl
+++ etc/index_packs.pl
@@ -6,28 +6,66 @@ use warnings;
 use lib 'lib';
 
 use File::Basename ();
+use Path::Class::Dir ();
 use SixteenColors;
 
-my $c     = 'SixteenColors';
-my $year  = shift;
-my @files = @ARGV;
-my $rs    = $c->model( 'DB::Pack' );
+my $c  = 'SixteenColors';
+my $rs = $c->model( 'DB::Pack' );
 
-unless ( $year and $year =~ m{^\d\d\d\d$} ) {
-    print "[ERROR] Invalid year specified\n";
-    exit;
+if( !@ARGV ) {
+    print <<"EOUSAGE"
+USAGE: $0 /path/to/archive/sorted/by/year/
+       $0 year /path/to/directory/
+       $0 year /path/to/file.ext
+EOUSAGE
 }
+elsif( -d $ARGV[ 0 ] ) {
+    my $dir = Path::Class::Dir->new( shift );
+    my @years = sort map { $_->basename }
+        grep { $_->is_dir && $_->basename =~ m{^\d{4}$} }
+        $dir->children( no_hidden => 1 );
+    _index_dir( $_, $dir->subdir( $_ ) ) for @years;
+}
+else {
+    my $year = shift;
+    my @files = @ARGV;
 
-for my $file ( @files ) {
-    printf "Indexing %s\n", $file;
-
-    my $basename = File::Basename::basename( $file );
-    if ( $rs->search( { filename => $basename } )->count ) {
-        printf
-            "[ERROR] A file of the same name (%s) has already been indexed\n",
-            $basename;
-        next;
+    unless ( $year =~ m{^\d\d\d\d$} ) {
+        print "[ERROR] Invalid year specified\n";
+        exit;
     }
 
-    $rs->new_from_file( $file, $year, $c );
+    if( @files == 1 && -d $files[ 0 ] ) {
+        _index_dir( $year, @files );
+    }
+    else {
+        _index( $year, @files );
+    }
+}
+
+sub _index_dir {
+    my $year  = shift;
+    my $dir   = Path::Class::Dir->new( shift );
+    my @files = map { $_->stringify } grep { !$_->is_dir }
+        $dir->children( no_hidden => 1 );
+    _index( $year, @files );
+}
+
+sub _index {
+    my( $year, @files ) = @_;
+
+    for my $file ( @files ) {
+        next if -d $file;
+        printf "Indexing %s\n", $file;
+
+        my $basename = File::Basename::basename( $file );
+        if ( $rs->search( { filename => $basename } )->count ) {
+            printf
+                "[ERROR] A file of the same name (%s) has already been indexed\n",
+                $basename;
+            next;
+        }
+
+        $rs->new_from_file( $file, $year, $c );
+    }
 }
