213a17462ac18ac7aea89c4aee53e6e6fa008d08
Brian Cassidy <bricas@cpan.org>
2011-09-27 21:37:20 -0300

    basic start of year api

---
+++ lib/SixteenColors/Schema/ResultSet/Year.pm
@@ -0,0 +1,15 @@
+package SixteenColors::Schema::ResultSet::Year;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet::Data::Pageset';
+
+sub TO_JSON {
+    my $self = shift;
+    return [
+        map { { $_->get_columns } } $self->all
+    ]
+}
+
+1;
