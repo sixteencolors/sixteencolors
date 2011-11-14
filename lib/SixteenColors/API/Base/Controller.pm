32fa1b97a0645fc0dae2f49f35dc25d93c03bdf5
Brian Cassidy <bricas@cpan.org>
2011-11-14 11:29:54 -0400

    page resultsets by default

--- lib/SixteenColors/API/Base/Controller.pm
+++ lib/SixteenColors/API/Base/Controller.pm
@@ -24,6 +24,28 @@ sub deserialize : ActionClass('Deserialize') {}
 
 sub end : Private {
     my( $self, $c ) = @_;
+    my $entity = $c->stash->{ $self->{'stash_key'} };
+
+    if( blessed $entity && $entity->isa( 'DBIx::Class::ResultSet' ) ) {
+        my $page = $c->req->params->{ page } || 1;
+        my $rows = $c->req->params->{ rows } || 30;
+        $rows = 100 if $rows > 100;
+
+        $entity = $entity->search( {}, { rows => $rows, page => $page } );
+        my $pager = $entity->pager;
+        $c->stash->{ $self->{'stash_key'} } = $entity;
+
+        my %paging = ( first => 'first', last => 'last', next => 'next', previous => 'prev' );
+        my @links;
+        for ( keys %paging ) {
+            my $m = "${_}_page";
+            next unless defined $pager->$m;
+            push @links, sprintf( qq(<%s>; rel="$paging{$_}"), $c->req->uri_with( { page => $pager->$m, rows => $rows } ) );
+        }
+
+        $c->res->header( 'Link' => join( ',', @links ) );
+    }
+
     $self->config->{ json_options }->{ pretty } = $c->is_development_server;
     $c->forward( 'serialize' );
     $c->res->content_type( $c->res->content_type . '; charset=utf-8' );
