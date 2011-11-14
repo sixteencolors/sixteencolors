00e8cfae7c94e141cbe80292c5f4de815f055fb7
Brian Cassidy <bricas@cpan.org>
2011-11-14 10:37:28 -0400

    JSONP support -- taken from Catalyst::Action::Serialize::JSONP

--- lib/Catalyst/Action/Serialize/SixteenColors/API/JSON.pm
+++ lib/Catalyst/Action/Serialize/SixteenColors/API/JSON.pm
@@ -16,4 +16,28 @@ before 'execute' => sub {
     }
 };
 
+after 'execute' => sub {
+    my $self = shift;
+    my ($controller, $c) = @_;
+
+    # Ripped from Catalyst::Action::Serialize::JSONP
+
+    my $callback_key = (
+        $controller->{ serialize } ?
+            $controller->{ serialize }->{ callback_key } :
+            $controller->{ callback_key }
+    ) || 'callback';
+
+    my $callback_value = $c->req->param( $callback_key );
+    if ( $callback_value ) {
+        if ($callback_value =~ /^\w+$/ ) {
+            $c->res->content_type( 'text/javascript' );
+            $c->res->output( $callback_value . '(' . $c->res->output() . ');' );
+        }
+        else {
+            warn 'Callback: '.$callback_value.' will not generate valid Javascript. Falling back to JSON output';
+        }
+    }
+};
+
 1;
