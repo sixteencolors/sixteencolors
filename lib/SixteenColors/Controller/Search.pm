c1e2acdc1ea5aea033708853911fc770fd98c3e0
Doug Moore <lordscarlet@idledreams.net>
2012-01-14 08:39:24 -0500

    Search results now handled by THE ALMIGHTY GOOGLE
    The filename will not appear over the images until Google's indexing
    catches up with the added microdata. closes #31

--- lib/SixteenColors/Controller/Search.pm
+++ lib/SixteenColors/Controller/Search.pm
@@ -2,59 +2,49 @@ package SixteenColors::Controller::Search;
 
 use Moose;
 use namespace::autoclean;
+use URI;
+use LWP::Simple;
+use XML::Simple;
+use Data::Dumper;
 
 BEGIN {
     extends 'Catalyst::Controller';
 }
 
 sub index : Path : Args(0) {
+    # 006684255443611537751:qjvj1fyr2ui Google API Key
+
+    # http://www.google.com/search?q=maestro&hl=en&start=10&num=10&output=xml&client=google-csbe&cx=006684255443611537751:qjvj1fyr2ui 
+
+
     my ( $self, $c ) = @_;
     $c->stash( title => 'Search' );
 
-    my $q = $c->req->params->{ q };
-    return unless $q;
-	
-	my $wildcard_search = $q =~ m{\*} ? 1 : 0;
+    my $key = "006684255443611537751:qjvj1fyr2ui";
+    my $page = $c->req->params->{ page } || 1;
+    my $results_per_page = 20; # Google will not return more than 20 per page
+    my $start = ($page - 1) * $results_per_page;
+    my $uri = URI->new("http://www.google.com/search?q=" . $c->req->params->{ q } . "&hl=en&start=" . $start . "&num=" . $results_per_page . "&output=xml&client=google-csbe&cx=" . $key);
+    my $xml = get $uri;
 
-	if ($wildcard_search) {
-		$q =~ s/\*/%/g;
-	}
-	
-    my $files = $c->model( 'DB::File' )->search_rs(
-        !$wildcard_search ? (
-			{   -or => [
-                'file_fulltext.fulltext' => { like => "%${q}%" },
-                'sauce'                  => { like => "%${q}%" },
-                'filename'               => { like => "%${q}%" }
-            ]
-        }) : (
-			{
-				'filename' => { like => $q}
-			}
-		),
-        {   join => 'file_fulltext',
-            page => $c->req->params->{ page } || 1,
-            rows => 25
-        }
-    );
+    my $xs = XML::Simple->new();
+    my $doc = $xs->XMLin($xml);
+
+    my $pageset = Data::Pageset->new({
+        'total_entries' => $doc->{RES}->{M},
+        'entries_per_page' => $doc->{PARAM}->{num}->{value},
+        'current_page' => $page
+    });
+
     $c->stash(
-        files  => $files,
-        pager  => $files->pageset,
-        groups => $c->model( 'DB::Group' )->search_rs(
-            {   -or => {
-                    name      => { like => "%${q}%" },
-                    shortname => { like => "%${q}%" }
-                }
-            }
-        ),
-        artists => $c->model( 'DB::Artist' )->search_rs(
-            {   -or => {
-                    name      => { like => "%${q}%" },
-                    shortname => { like => "%${q}%" }
-                }
-            }
-        ),
-        template => 'search/results.tt'
+        pager => $pageset,
+        template => 'search/results.tt',
+        files => $doc->{RES}->{R},
+        uri => $uri,
+        next => $doc->{RES}->{NB}->{NU},
+        prev => $doc->{RES}->{NB}->{PU}
+    );
+
     );
 }
 
