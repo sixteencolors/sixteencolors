bfa9eeef2a840653faa7c0ef16d8f03e34c94193
Brian Cassidy <bricas@cpan.org>
2012-01-16 15:50:51 -0400

    add utility to generate a sitemap

---
+++ etc/generate_sitemap.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib';
+
+use SixteenColors;
+
+my %MAPS  = (
+    dynamic => [ qw( artist group file pack ) ],
+    static  => [ qw( pages ) ],
+);
+    
+my $LIMIT = 50_000;
+$Template::Directive::WHILE_MAX = $LIMIT + 2;
+
+my $c     = 'SixteenColors';
+my $view  = $c->view( 'HTML' );
+my $path  = $c->path_to( 'root/static' )->subdir( 'sitemap' );
+$path->mkpath;
+my %pages = ( static => [], dynamic => {} );
+
+# DYNAMIC
+for ( @{ ${ MAPS }{ dynamic } } ) {
+    my $rs = $c->model( 'DB::' . ucfirst( $_ ) )->search( {}, { page => 1, rows => $LIMIT } );
+    my $pager = $rs->pager;
+    $pages{ dynamic }->{ $_ } = $pager->last_page;
+    my $line = sprintf( "\r[INFO] ${_}s (%%d/%d)", $pager->last_page );
+
+    for my $page ( 1..$pager->last_page ) {
+        printf STDERR $line, $page;
+        my $items = $rs->page( $page );
+        my $file = $path->file( "${_}${page}.xml" );
+        my $fh = $file->openw;
+        print $fh $view->render( undef, "sitemap/${_}.tt", { no_wrapper => 1, items => $items } );
+        close $fh;
+    }
+    print "\n";
+}
+
+# STATIC
+for ( @{ ${ MAPS }{ static } } ) {
+    print STDERR "\r[INFO] ${_}";
+    push @{ $pages{ static } }, $_;
+    my $file = $path->file( "${_}.xml" );
+    my $fh = $file->openw;
+    print $fh $view->render( undef, "sitemap/${_}.tt", { no_wrapper => 1 } );
+    close $fh;
+    print "\n";
+}
+
+# INDEX
+print STDERR "\r[INFO] index";
+my $file = $path->file( "index.xml" );
+my $fh = $file->openw;
+print $fh $view->render( undef, "sitemap/index.tt", { no_wrapper => 1, pages => \%pages } );
+close $fh;
+print "\n";
