52908776e49ae8e47c6efb851cec55ef86b9c704
Brian Cassidy <brian.cassidy@gmail.com>
2015-01-20 14:53:06 -0400

    add tracker to mp3 conversion

--- lib/SixteenColors/FileType/Tracker.pm
+++ lib/SixteenColors/FileType/Tracker.pm
@@ -6,7 +6,15 @@ use warnings;
 use parent 'SixteenColors::FileType';
 
 sub generate_surrogates {
-    my( $self, $c ) = @_;
+    my( $self, $c, $file_db ) = @_;
+    my $file = $self->filename;
+    my $basename = $file_db->filename;
+
+    # Only full images for now
+    my $destfile = $c->path_to( 'root', 'static', 'audio', $file_db->pack->shortname, "${basename}.mp3" );
+    $destfile->dir->mkpath;
+
+    system( "xvfb-run vlc -q -Idummy ${file} --sout '#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100}:file{dst=${destfile}}' vlc://quit >/dev/null 2>&1" );
 }
 
 1;
