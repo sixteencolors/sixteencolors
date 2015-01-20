8be169e8165044d8d5995fcefcf40d9538960c31
Brian Cassidy <brian.cassidy@gmail.com>
2015-01-20 12:08:23 -0400

    fix textmode output

--- lib/SixteenColors/FileType/Textmode.pm
+++ lib/SixteenColors/FileType/Textmode.pm
@@ -29,7 +29,7 @@ sub generate_surrogates {
 
     my $fh = $destfile->open( 'w' ) or die "cannot write file ($destfile): $!";
     binmode( $fh );
-    print $fh $raw;
+    print $fh $raw->png;
     close( $fh );
 }
 
