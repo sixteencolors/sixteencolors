402c5f63bf4f691f1c1ceed6a45e82541e8dec11
Brian Cassidy <brian.cassidy@gmail.com>
2015-01-22 14:46:24 -0400

    simple copy for bitmap images

--- lib/SixteenColors/FileType/Bitmap.pm
+++ lib/SixteenColors/FileType/Bitmap.pm
@@ -5,8 +5,17 @@ use warnings;
 
 use parent 'SixteenColors::FileType';
 
+use File::Copy ();
+
 sub generate_surrogates {
-    my( $self, $c ) = @_;
+    my( $self, $c, $file_db ) = @_;
+    my $file = $self->filename;
+    my $basename = $file_db->filename;
+
+    # Only full images for now
+    my $destfile = $c->path_to( 'root', 'static', 'images', 'f', $file_db->pack->shortname, $basename );
+    $destfile->dir->mkpath;
+    File::Copy::copy( "$file", "$destfile" );
 }
 
 1;
